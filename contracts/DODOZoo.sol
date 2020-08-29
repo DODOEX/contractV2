2191493ef616be4e74d7bf52f00619188e0e11a3
mingda <leimingda@pku.edu.cn>
2020-08-29 18:31:23 +0800

    fix remove DODO

--- contracts/DODOZoo.sol
+++ contracts/DODOZoo.sol
@@ -12,6 +12,7 @@ import {Ownable} from "./lib/Ownable.sol";
 import {IDODO} from "./intf/IDODO.sol";
 import {ICloneFactory} from "./helper/CloneFactory.sol";
 
+
 /**
  * @title DODOZoo
  * @author DODO Breeder
@@ -62,7 +63,7 @@ contract DODOZoo is Ownable {
         address quoteToken = IDODO(dodo)._QUOTE_TOKEN_();
         require(isDODORegistered(baseToken, quoteToken), "DODO_NOT_REGISTERED");
         _DODO_REGISTER_[baseToken][quoteToken] = address(0);
-        for (uint256 i = 0; i < _DODOs.length - 1; i++) {
+        for (uint256 i = 0; i <= _DODOs.length - 1; i++) {
             if (_DODOs[i] == dodo) {
                 _DODOs[i] = _DODOs[_DODOs.length - 1];
                 _DODOs.pop();
