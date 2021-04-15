90cca518bab98ec85444931b336b8ae341747bbd
owen05 <liuhongnan53@163.com>
2021-04-15 12:19:51 +0800

    update initializableERC721 && ERC1155

--- contracts/external/ERC721/InitializableERC721.sol
+++ contracts/external/ERC721/InitializableERC721.sol
@@ -7,18 +7,18 @@
 
 pragma solidity 0.6.9;
 
-import {ERC721} from "./ERC721.sol";
+import {ERC721URIStorage} from "./ERC721URIStorage.sol";
 
-contract InitializableERC721 is ERC721 {
+contract InitializableERC721 is ERC721URIStorage {
     function init(
         address creator,
         string memory name,
         string memory symbol,
-        string memory baseUri
+        string memory uri
     ) public {
         _name = name;
         _symbol = symbol;
-        _baseUri = baseUri;
         _mint(creator, 0);
+        _setTokenURI(0, uri);
     }
 }
