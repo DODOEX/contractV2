9499a7ea839736822a379d9ec1bf09322769f3b6
Attens1423 <attens1423@outlook.com>
2021-05-28 17:07:23 +0800

    heco new adapter

--- migrations/6_deploy_periphery.js
+++ migrations/6_deploy_periphery.js
@@ -38,7 +38,10 @@ const DODOMineV3Registry = artifacts.require("DODOMineV3Registry");
 const DODOMineV3Proxy = artifacts.require("DODOMineV3Proxy");
 
 
-const CurveAdapter = artifacts.require("CurveAdapter");
+const CurveAdapter = artifacts.require("CurveUnderlyingAdapter");
+const DODOV1Adapter = artifacts.require("DODOV1Adapter");
+const DODOV2Adapter = artifacts.require("DODOV2Adapter");
+const UniAdapter = artifacts.require("UniAdapter");
 
 module.exports = async (deployer, network, accounts) => {
     let CONFIG = GetConfig(network, accounts)
@@ -48,6 +51,8 @@ module.exports = async (deployer, network, accounts) => {
     let DODOTokenAddress = CONFIG.DODO;
     let DODOApproveProxyAddress = CONFIG.DODOApproveProxy;
     let WETH = CONFIG.WETH;
+
+    let DODOSellHelperAddress = CONFIG.DODOSellHelper;
 
     let DspTemplateAddress = CONFIG.DSP;
     let DspFactoryAddress = CONFIG.DSPFactory;
@@ -492,5 +497,17 @@ module.exports = async (deployer, network, accounts) => {
         await deployer.deploy(CurveAdapter);
 
         logger.log("test_Adapter Address: ", CurveAdapter.address);
+
+        logger.log("====================================================");
+        logger.log("network type: " + network);
+        logger.log("Deploy time: " + new Date().toLocaleString());
+        logger.log("Deploy type: anothertest - Adapter");
+
+        await deployer.deploy(DODOV1Adapter, DODOSellHelperAddress)
+        logger.log("DODOV1Adapter Address: ", DODOV1Adapter.address);
+        await deployer.deploy(DODOV2Adapter)
+        logger.log("DODOV2Adapter Address: ", DODOV2Adapter.address);
+        await deployer.deploy(UniAdapter)
+        logger.log("UniAdapter Address: ", UniAdapter.address);
     }
 };
