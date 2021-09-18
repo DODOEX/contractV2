ed7481f2b9059489588a2ae9cbb822c9ed969d79
owen05 <liuhongnan53@163.com>
2021-09-18 11:04:49 +0800

    deploy moonriver

--- config/moonriver-config.js
+++ config/moonriver-config.js
@@ -8,8 +8,8 @@ module.exports = {
         //Helper
         //MultiCall: "0xA922EeEB7909E4a06DCb4b2bff0335Cd027cCfbF",
         //DODOSwapCalcHelper: "0xA45b1105d21dFd3915EaDF1b5523196B458C7614",
-        //DODOZoo: "",
-        //DODO: "",
+        //DODOZoo: "0x02fCB21dc1cf221939C1d4277fB54016b5d32bC7",//not verify
+        //DODO: "0xB5397B2210f49e96a5EB2c9747Aa2bD9397d90C0",//not verify
         //ERC20Helper: "0x6373ceB657C83C91088d328622573FB766064Ac4", //not verify
         DODOSellHelper: "0x5e84190a270333aCe5B9202a3F4ceBf11b81bB01", //not verify
         DODOCalleeHelper: "0x6B208E08dcF6BD51F50C5Da09d15B2D8E5C46Cf2", //not verify
@@ -24,14 +24,15 @@ module.exports = {
         DVM: "0xC3BeD579CaB3EC29B22D9AB99F4E586af42496b9",//not verify
         DPP: "0x989DcAA95801C527C5B73AA65d3962dF9aCe1b0C",//not verify
         DSP: "0x550B2e7bD9605b8dcdd20d01bA73f1feB6ce289b",
-        DPPAdmin: "0x041ABa00c57Dd47abC37A2931dF569a2A2cc57Be",
+        DPPAdmin: "0x041ABa00c57Dd47abC37A2931dF569a2A2cc57Be",//not verify
         CP: "0x72d220cE168C4f361dD4deE5D826a01AD8598f6C",
-        ERC20MineV3: "0xf50BDc9E90B7a1c138cb7935071b85c417C4cb8e",
+        ERC20MineV3: "0xf50BDc9E90B7a1c138cb7935071b85c417C4cb8e",//not verify
 
-        ERC20: "0x80930Cb1849F7D42531506fF45E66724338A821b",
-        MintableERC20: "",
-        CustomERC20: "0xB5Dc5E183c2aCf02aB879A8569aB4EDAf147d537",
+        ERC20: "0x80930Cb1849F7D42531506fF45E66724338A821b",//not verify
+        CustomERC20: "0xB5Dc5E183c2aCf02aB879A8569aB4EDAf147d537",//not verify
 
+
+        //not verify
         //Factory
         DVMFactory: "0x738Ebf387A0CE0eb46b0eF8Fa5DEa2EaE6B1Df51",
         DPPFactory: "0xb57Dd5c265dBb13CA014F2332069E90CD0e22e65",
@@ -41,11 +42,13 @@ module.exports = {
         ERC20V2Factory: "0xaeB5CF31b97dce6134e416129845e01106fFB177",
         DODOMineV3Registry: "0x5515363c0412AdD5c72d3E302fE1bD7dCBCF93Fe",
 
+        //not verify
         //Approve
         DODOApprove: "0xE8C9A78725D0451FA19878D5f8A3dC0D55FECF25",
         DODOApproveProxy: "0x7737fd30535c69545deeEa54AB8Dd590ccaEBD3c",
 
 
+        //not verify
         //Adapter
         DODOV1Adapter: "0x40672211D4310ad71daDc8cDE7Aa3Fb90d420855",
         DODOV2Adapter: "0xbe9a66e49503e84ae59a4d0545365AABedf33b40",
@@ -53,6 +56,7 @@ module.exports = {
         CurveAdapter: "",
 
 
+        //not verify
         //Proxy
         DODOV2Proxy: "0xa2CB66EBB947D217f61510882096F6e95c1DE97D",
         DSPProxy: "0x5480B32c03647ff5E5A653F0465E798DBe558B57",
