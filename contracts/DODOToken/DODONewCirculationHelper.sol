c2023f3dca4e64fbc031a9029d1cefb85a642bde
Attens1423 <attens1423@outlook.com>
2023-11-08 22:13:45 +0800

    add new vDODOCirculationHelper

---
+++ contracts/DODOToken/DODONewCirculationHelper.sol
@@ -0,0 +1,16 @@
+/*
+
+    Copyright 2020 DODO ZOO.
+    SPDX-License-Identifier: Apache-2.0
+
+*/
+pragma solidity 0.6.9;
+pragma experimental ABIEncoderV2;
+
+contract DODONewCirculationHelper {
+
+    function getDodoWithdrawFeeRatio() external pure returns (uint256 ratio) {
+        return 0;
+    }
+
+}
